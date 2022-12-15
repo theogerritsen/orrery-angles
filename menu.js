c94993e028115fa32ed37f52fcfd5dd824654c04
Theo Gerritsen <theo.gerritsen0703@gmail.com>
2022-12-15 14:46:30 +0100

    js corrigé

--- menu.js
+++ menu.js
@@ -1,96 +1,96 @@
-let sidebarBox = document.querySelector('#box');
-let sidebarBtn = document.querySelector('#btn');
+// let sidebarBox = document.querySelector('#box');
+// let sidebarBtn = document.querySelector('#btn');
 
-sidebarBtn.addEventListener('click', function(event) {
+// sidebarBtn.addEventListener('click', function(event) {
 
-		if (this.classList.contains('active')) {
-				this.classList.remove('active');
-				sidebarBox.classList.remove('active');
-		} else {
-				this.classList.add('active');
-				sidebarBox.classList.add('active');
-		}
-});
+// 		if (this.classList.contains('active')) {
+// 				this.classList.remove('active');
+// 				sidebarBox.classList.remove('active');
+// 		} else {
+// 				this.classList.add('active');
+// 				sidebarBox.classList.add('active');
+// 		}
+// });
 
-window.addEventListener('keydown', function(event) {
+// window.addEventListener('keydown', function(event) {
 
-		if (sidebarBox.classList.contains('active') && event.keyCode === 27) {
-				sidebarBtn.classList.remove('active');
-				sidebarBox.classList.remove('active');
-		}
-});
-// changer le chemin selon le fichier
-// on va chercher le nom du fichier dans lequel on est actuellement
-let tenmeter = document.querySelector('#tenmeters');
-tenmeter.addEventListener('click', function() {
-    if($(this).is(":checked")) {
-        $("#_10p").show();
-        $("#_10m").show();
-    } else {
-        $("#_10p").hide();
-        $("#_10m").hide();
-    }
-});
-let five = document.querySelector('#fivemeters');
+// 		if (sidebarBox.classList.contains('active') && event.keyCode === 27) {
+// 				sidebarBtn.classList.remove('active');
+// 				sidebarBox.classList.remove('active');
+// 		}
+// });
+// // changer le chemin selon le fichier
+// // on va chercher le nom du fichier dans lequel on est actuellement
+// let tenmeter = document.querySelector('#tenmeters');
+// tenmeter.addEventListener('click', function() {
+//     if($(this).is(":checked")) {
+//         $("#_10p").show();
+//         $("#_10m").show();
+//     } else {
+//         $("#_10p").hide();
+//         $("#_10m").hide();
+//     }
+// });
+// let five = document.querySelector('#fivemeters');
 
-five.addEventListener('click', function() {
-    if($(this).is(":checked")) {
-        $("#_5p").show();
-        $("#_5m").show();
-    } else {
-        $("#_5p").hide();
-        $("#_5m").hide();
-    }
-});
-let one = document.querySelector('#onemeter');
-one.addEventListener('click', function() {
-    if($(this).is(":checked")) {
-        $("#_1p").show();
-    } else {
-        $("#_1p").hide();
-    }
-});
-let labels = document.querySelector('#labels');
-labels.addEventListener('click', function() {
-    if($(this).is(":checked")) {
-        $("#_10m").show();
-        $("#_5m").show();
-    } else {
-        $("#_10m").hide();
-        $("#_5m").hide();
-    }
-});
-let circle = document.querySelector('#circle');
-circle.addEventListener('click', function() {
-    if($(this).is(":checked")) {
-        $("#cercle").show();
-    } else {
-        $("#cercle").hide();
-    }
-});
+// five.addEventListener('click', function() {
+//     if($(this).is(":checked")) {
+//         $("#_5p").show();
+//         $("#_5m").show();
+//     } else {
+//         $("#_5p").hide();
+//         $("#_5m").hide();
+//     }
+// });
+// let one = document.querySelector('#onemeter');
+// one.addEventListener('click', function() {
+//     if($(this).is(":checked")) {
+//         $("#_1p").show();
+//     } else {
+//         $("#_1p").hide();
+//     }
+// });
+// let labels = document.querySelector('#labels');
+// labels.addEventListener('click', function() {
+//     if($(this).is(":checked")) {
+//         $("#_10m").show();
+//         $("#_5m").show();
+//     } else {
+//         $("#_10m").hide();
+//         $("#_5m").hide();
+//     }
+// });
+// let circle = document.querySelector('#circle');
+// circle.addEventListener('click', function() {
+//     if($(this).is(":checked")) {
+//         $("#cercle").show();
+//     } else {
+//         $("#cercle").hide();
+//     }
+// });
     
-$(function() {
-    $("#sentier-btn").click(function() {
-        window.location.href = "tracking.html"
-    });
-    $("#jeu-btn").click(function() {
-        document.querySelector('.inst-tab').classList.toggle("active");
-            document.querySelector('.img-tab').classList.toggle("active");
-    });
-    $("#img-btn").click(function() {
-        document.querySelector('.img-tab').classList.toggle("active");
-                // si le score est déjà affiché alors on ne toggle pas la classe "active"
-        // pour éviter qu'il disparaisse
-        if (document.querySelector('.score').classList.contains("active")) {
-            return
-        } else {
-            document.querySelector('.score').classList.toggle("active");
-        };
-    });
-    $(".bx-menu").click(function() {
-        document.querySelector('.menu').classList.toggle("active");
-    });
-});
+// $(function() {
+//     $("#sentier-btn").click(function() {
+//         window.location.href = "tracking.html"
+//     });
+//     $("#jeu-btn").click(function() {
+//         document.querySelector('.inst-tab').classList.toggle("active");
+//             document.querySelector('.img-tab').classList.toggle("active");
+//     });
+//     $("#img-btn").click(function() {
+//         document.querySelector('.img-tab').classList.toggle("active");
+//                 // si le score est déjà affiché alors on ne toggle pas la classe "active"
+//         // pour éviter qu'il disparaisse
+//         if (document.querySelector('.score').classList.contains("active")) {
+//             return
+//         } else {
+//             document.querySelector('.score').classList.toggle("active");
+//         };
+//     });
+//     $(".bx-menu").click(function() {
+//         document.querySelector('.menu').classList.toggle("active");
+//     });
+// });
 
 let scene = document.querySelector('#etape3');
 
